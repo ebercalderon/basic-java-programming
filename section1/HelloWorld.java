af4e3db6046872a26c1e072d42fd2de76c6bfaf1
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-13 23:07:23 -0500

    Add new folder section1

---
+++ section1/HelloWorld.java
@@ -0,0 +1,7 @@
+package section1;
+
+public class HelloWorld { // Defines the HelloWorld class
+    public static void main(String[] args) { // Defines the main method
+        System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+    }
+}
