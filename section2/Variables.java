211492e549cedf2f96ab491adda960e0db54b7fd
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-13 23:09:54 -0500

    Add new folder section2

---
+++ section2/Variables.java
@@ -0,0 +1,49 @@
+package section2;
+
+import java.util.Scanner;
+
+public class Variables {
+
+    /**
+     * ctrl + CLICK : variable definition location ctrl + SPACE : variable selection psvm + TAB sout + TAB
+     */
+    public static void main(String[] args) {
+        String title = "Book";
+        String author = "Eber";
+        System.out.println(title + " was written by " + author);
+
+
+        // variable definition
+        int myIntegerVariable = 10;
+        System.out.println(myIntegerVariable);
+
+        // variable value modification
+        myIntegerVariable = 5;
+        System.out.println(myIntegerVariable);
+
+        String myStringVariable = "greetings";
+        System.out.println(myStringVariable);
+
+        // var - type inference in java
+        var myIntegerVariable2 = 15;
+        System.out.println(myIntegerVariable2);
+
+        /**
+         * Rules for defining variables
+         * var myVariable = 1;
+         * var _myVariable = 2;
+         * var $myVariable = 3:
+         */
+
+        // Concatenation: Left to right evaluation (string context) and parenthesis (priority)
+        // Special characters: \n,\t,\b, \', \"
+
+        // Scanner class
+        System.out.println("Enter your name: ");
+        Scanner console = new Scanner(System.in);
+        var user = console.nextLine();
+        System.out.println("user = " + user); // soutv + TAB
+
+    }
+
+}
