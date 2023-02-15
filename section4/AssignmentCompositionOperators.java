6993dcbf171907edd2d0a62ab7e378c66b66f2c9
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 16:22:02 -0500

    Assignment and Composition Operators

---
+++ section4/AssignmentCompositionOperators.java
@@ -0,0 +1,22 @@
+package section4;
+
+public class AssignmentCompositionOperators {
+
+    public static void main(String[] args) {
+        int a = 3, b = 2;
+        int c = a + 5 - b;
+        System.out.println("c = " + c);
+
+        a += 1;//a = a + 1
+        System.out.println("a = " + a);
+
+        a += 3;//a = a + 3
+        System.out.println("a = " + a);
+
+        a -= 2;//a = a - 2
+        System.out.println("a = " + a);
+
+        // *=   /=   %=
+    }
+
+}
