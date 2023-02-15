b282219da6a4ae8f50e7dd8b7db8c2556c20ae6b
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-14 20:42:09 -0500

    Primitive Boolean Value

---
+++ section3/PrimitiveBooleanValue.java
@@ -0,0 +1,26 @@
+package section3;
+
+public class PrimitiveBooleanValue {
+
+    public static void main(String args[]) {
+        boolean varBoolean = true;
+        System.out.println("varBoolean = " + varBoolean);
+
+        if(varBoolean){
+            System.out.println("The flag is true");
+        }
+        else{
+            System.out.println("The flag is false");
+        }
+
+        int age = 10;
+        //boolean isAdult = age >= 18;
+        if(age >= 18 ){
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are a minor");
+        }
+    }
+
+}
