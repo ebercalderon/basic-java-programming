67d6045c1a8d907b72d518a930b980130d132efb
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 18:40:23 -0500

    Precedence of operators

---
+++ section4/PrecedenceOperators.java
@@ -0,0 +1,20 @@
+package section4;
+
+public class PrecedenceOperators {
+
+    public static void main(String args[]) {
+        var x = 5;
+        var y = 10;
+        var z = ++x + y--;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+
+        var result = 4 + 5 * 6 / 3;//4 + ((5*6)/3)
+        System.out.println("result = " + result);//14
+
+        result = (4 + 5) * 6 / 3;
+        System.out.println("result = " + result);
+    }
+
+}
