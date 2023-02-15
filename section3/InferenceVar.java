177077b2399dd2f121ebd177bee1a592b72c6fd5
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-14 20:01:21 -0500

    Inference Var

---
+++ section3/InferenceVar.java
@@ -0,0 +1,16 @@
+package section3;
+
+public class InferenceVar {
+
+    public static void main(String args[]) {
+        var integerNumber = 10;
+        System.out.println("integerNumber = " + integerNumber);
+
+        var doubleNumber = 10.0;
+        System.out.println("doubleNumber = " + doubleNumber);
+
+        var floatNumber = 10.0F;
+        System.out.println("floatNumber = " + floatNumber);
+    }
+
+}
