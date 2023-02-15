3d4afc43778b2bb98b4723b66aea4c74a17232e3
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-14 20:46:33 -0500

    Section 1 completed

--- section1/HelloWorld.java
+++ section1/HelloWorld.java
@@ -2,6 +2,6 @@ package section1;
 
 public class HelloWorld { // Defines the HelloWorld class
     public static void main(String[] args) { // Defines the main method
-        System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!"); // Prints "Hello, World!" in console
     }
 }
