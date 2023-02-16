c4c3a196562037138d0e38acf552ce6f865afade
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 20:00:33 -0500

    For Cycle

---
+++ section6/ForCycle.java
@@ -0,0 +1,26 @@
+package section6;
+
+public class ForCycle {
+
+    public static void main(String[] args) {
+        /*
+        var counter = 0;
+        while (counter < 3){
+            System.out.println("counter = " + counter);
+            counter++;
+        }
+
+
+        var counter = 0;
+        do{
+            System.out.println("counter = " + counter);
+            counter++;
+        }while( counter < 3);
+        */
+
+        for( var counter = 0 ; counter < 3 ; counter++ ){
+            System.out.println("counter = " + counter);
+        }
+    }
+
+}
