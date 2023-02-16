cc0e480935a2b3aeb392511dfd98c14147e4acdf
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 19:58:47 -0500

    Do While Cycle

---
+++ section6/DoWhileCycle.java
@@ -0,0 +1,21 @@
+package section6;
+
+public class DoWhileCycle {
+
+    public static void main(String[] args) {
+        /*
+        var counter = 0;
+        while (counter < 3){
+            System.out.println("counter = " + counter);
+            counter++;
+        }
+        */
+
+        var counter = 0;
+        do{
+            System.out.println("counter = " + counter);
+            counter++;
+        }while( counter < 3);
+    }
+
+}
