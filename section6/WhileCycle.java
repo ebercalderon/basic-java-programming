0c83bdb29966918ee9adea19f22e6d5de7c58ce0
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 19:24:56 -0500

    While Cycle

---
+++ section6/WhileCycle.java
@@ -0,0 +1,13 @@
+package section6;
+
+public class WhileCycle {
+
+    public static void main(String[] args) {
+        var counter = 0;
+        while (counter < 3){
+            System.out.println("counter = " + counter);
+            counter++;
+        }
+    }
+
+}
