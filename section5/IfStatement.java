8f595341e6e29158b8c727113423869e1c5da628
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 19:00:17 -0500

    if statement

---
+++ section5/IfStatement.java
@@ -0,0 +1,15 @@
+package section5;
+
+public class IfStatement {
+
+    public static void main(String[] args) {
+        boolean condition = true;
+
+        if (condition) {
+            System.out.println("True condition");
+        } else {
+            System.out.println("False condition");
+        }
+    }
+
+}
