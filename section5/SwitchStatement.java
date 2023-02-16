54ba9535f24ea6689db7fcd5828f17daaf1a3bfe
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 19:15:14 -0500

    switch statement

---
+++ section5/SwitchStatement.java
@@ -0,0 +1,28 @@
+package section5;
+
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+        int number = 10;
+        String numberText = "Unknown value";
+
+        switch(number){
+            case 1:
+                numberText = "Number one";
+                break;
+            case 2:
+                numberText = "Number two";
+                break;
+            case 3:
+                numberText = "Number three";
+                break;
+            case 4:
+                numberText = "Number four";
+                break;
+            default:
+                numberText = "Case not found";
+        }
+        System.out.println("numberText = " + numberText);
+    }
+
+}
