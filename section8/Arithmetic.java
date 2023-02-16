0b0c6eb2c9ad35e18101a7be0c4b2d13c2f8b9b5
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 20:54:51 -0500

    Arithmetic class

---
+++ section8/Arithmetic.java
@@ -0,0 +1,27 @@
+package section8;
+
+public class Arithmetic {
+    // Class attributes
+    int a;
+    int b;
+
+    //Method
+    public void add() {
+        int result = a + b;
+        System.out.println("result = " + result);
+    }
+
+    public int addWithReturn() {
+        // int result = a + b;
+        // return result;
+        return this.a + this.b;
+
+    }
+
+    public int addWithArguments(int a, int b){
+        this.a = a; // The argument an is assigned to the attribute this.a
+        this.b = b;
+        //return a + b;
+        return this.addWithReturn();
+    }
+}
