a0dbc682f4e5c712c881712c097fa6848938f908
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-15 20:39:44 -0500

    Test Person class

---
+++ section7/classes/TestPerson.java
@@ -0,0 +1,22 @@
+package section7.classes;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Juan";
+        person1.lastName = "Perez";
+        person1.displayInformation();
+
+        Person person2 = new Person();
+
+        System.out.println("person1 = " + person1);
+        System.out.println("person2 = " + person2);
+
+        person2.displayInformation();
+        person2.name = "Karla";
+        person2.lastName = "Lara";
+        person2.displayInformation();
+    }
+
+}
