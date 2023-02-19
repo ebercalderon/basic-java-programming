6836c049322d5af682d631df8a83af958d9b5e2b
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-18 21:34:05 -0500

    Section 13 completed

--- section13/WordThis.java
+++ section13/WordThis.java
@@ -4,7 +4,7 @@ public class WordThis {
 
     public static void main(String[] args) {
 //        Person person1 = new Person( new String("Juan"), "Perez");
-        Person person1 = new Person( "Juan", "Perez");
+        Person person1 = new Person("Juan", "Perez");
         System.out.println("person1 = " + person1);
         System.out.println("person1 name = " + person1.name);
         System.out.println("person1 last name = " + person1.lastName);
@@ -14,6 +14,7 @@ public class WordThis {
 }
 
 class Person {
+    // super(); call to the constructor of the Parent class (object)
     String name;
     String lastName;
 
@@ -21,5 +22,20 @@ class Person {
         this.name = name;
         this.lastName = lastName;
         System.out.println("person object using this = " + this);
+//        Print print = new Print();
+        new Print().print(this);
     }
+}
+
+class Print {
+
+    public Print() {
+        super(); // call to the constructor of the Parent class (object)
+    }
+
+    public void print(Person person) {
+        System.out.println("person from print = " + person);
+        System.out.println("print current object (this) = " + this);
+    }
+
 }
