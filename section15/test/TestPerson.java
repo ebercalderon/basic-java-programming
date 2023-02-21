a315ba7465f2ec0754ca7df68f79c7ff9c2bd905
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-20 21:22:59 -0500

    Section 15 completed

--- section15/test/TestPerson.java
+++ section15/test/TestPerson.java
@@ -3,13 +3,24 @@ package section15.test;
 import section15.domain.Person;
 
 public class TestPerson {
+    private int counter;
 
     public static void main(String[] args) {
         Person person1 = new Person("Juan");
-        System.out.println("person1 = " + person1.toString());
-
         Person person2 = new Person("Karla");
-        System.out.println("person2 = " + person2.toString());
+
+        print(person1);
+        print(person2);
+
+        //this.counter = 10;
     }
 
+    public static void print(Person person) {
+        System.out.println("person = " + person);
+    }
+
+    public int getCounter() {
+        print(new Person("Carlos"));
+        return this.counter;
+    }
 }
