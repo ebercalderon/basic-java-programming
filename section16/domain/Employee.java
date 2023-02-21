277eb8c9df72e985ee5414506a02045924502cb9
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-20 23:29:57 -0500

    Using Super()

--- section16/domain/Employee.java
+++ section16/domain/Employee.java
@@ -1,4 +1,39 @@
 package section16.domain;
 
 public class Employee extends Person{
+    private int idEmployee;
+    private double salary;
+    private static int employeeCounter;
+
+    public Employee(String name, double salary) {
+        super(name);
+        this.idEmployee = ++Employee.employeeCounter;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public static int getEmployeeCounter() {
+        return employeeCounter;
+    }
+
+    public static void setEmployeeCounter(int employeeCounter) {
+        Employee.employeeCounter = employeeCounter;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employee{");
+        sb.append("idEmployee=").append(this.idEmployee);
+        sb.append(", salary=").append(this.salary);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- section16/test/TestInheritance.java
+++ section16/test/TestInheritance.java
@@ -5,7 +5,7 @@ import section16.domain.Employee;
 public class TestInheritance {
 
     public static void main(String[] args) {
-        Employee employee1 = new Employee();
+        Employee employee1 = new Employee("Carlos", 1025);
         System.out.println("employee1 = " + employee1);
     }
 
