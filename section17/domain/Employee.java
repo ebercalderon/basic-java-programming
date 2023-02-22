be8748bb3f36560f9acf54e488174366dc41f7ea
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-21 19:53:30 -0500

    Function overloading

---
+++ section17/domain/Employee.java
@@ -0,0 +1,47 @@
+package section17.domain;
+
+import section16.domain.Person;
+
+public class Employee extends Person {
+    private int idEmployee;
+    private double salary;
+    private static int employeeCounter;
+
+    public Employee() {
+        //super();
+        this.idEmployee = ++Employee.employeeCounter;
+    }
+
+    public Employee(String name, double salary) {
+        //super(name);
+        this();
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public static int getEmployeeCounter() {
+        return employeeCounter;
+    }
+
+    public static void setEmployeeCounter(int employeeCounter) {
+        Employee.employeeCounter = employeeCounter;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employee{");
+        sb.append("idEmployee=").append(this.idEmployee);
+        sb.append(", salary=").append(this.salary);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ section17/test/TestOverload.java
@@ -0,0 +1,16 @@
+package section17.test;
+
+import section17.domain.Employee;
+
+public class TestOverload {
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Carlos", 1025);
+        System.out.println("employee1 = " + employee1);
+//        var date = new Date();
+//
+//        Customer customer1 = new Customer("Juan", 'M', 18, "742 Evergreen Terrace", date, true);
+//        System.out.println("customer1 = " + customer1);
+    }
+
+}
