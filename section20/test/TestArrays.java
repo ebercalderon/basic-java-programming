5a0fd51ba82a4e259625a72f2e6691557f92c9c6
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-22 20:12:46 -0500

    Arrays

---
+++ section20/test/TestArrays.java
@@ -0,0 +1,15 @@
+package section20.test;
+
+public class TestArrays {
+
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+        System.out.println("ages = " + ages);
+
+        ages[0] = 10;
+        System.out.println("ages 0 = " + ages[0]);
+
+        // ages[3] = 5;
+    }
+
+}
