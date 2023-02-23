db4fc285630ae181665408068387b40bf547b76a
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-23 01:13:43 -0500

    Product class

---
+++ section22/pe/com/sales/Product.java
@@ -0,0 +1,47 @@
+package section22.pe.com.sales;
+
+public class Product {
+    private final int idProduct;
+    private String name;
+    private double price;
+    private static int counterProduct;
+
+    public Product(){
+        this.idProduct = ++Product.counterProduct;
+    }
+
+    public Product(String name, double price) {
+        this();
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "idProduct=" + idProduct +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
